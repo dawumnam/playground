aad407c3c07b4e2621a260cf7edbe3778448ea27
Dawn <42849054+dawumnam@users.noreply.github.com>
2022-06-01 15:45:58 +0900

    1 test (#2)
    * test
    
    * test

--- javascript/index.js
+++ javascript/index.js
@@ -1,91 +1,91 @@
-// UV_THREADPOOL_SIZE = 100;
-// const crypto = require("crypto");
+UV_THREADPOOL_SIZE = 100;
+const crypto = require("crypto");
 
-// const testPerson = { name: "dawn", age: 34, height: 173 };
+const testPerson = { name: "dawn", age: 34, height: 173 };
 
-// const incrementHeight = (person) => {
-//   const _person = person;
-//   _person.height++;
-//   return _person;
-// };
-// console.log("1");
-// (async () => {
-//   return await new Promise((resolve) => {
-//     // crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
-//     //   console.log("P1");
-//     //   resolve();
-//     // });
-//     console.log("P1");
-//     resolve();
-//   });
-// })();
-
-// () => {
-//   crypto.pbkdf2Sync();
-// };
-
-// console.log("2");
-// (async () => {
-//   return await new Promise((resolve) => {
-//     // crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
-//     //   console.log("P2");
-//     //   resolve();
-//     // });
-//     console.log("P1");
-//   });
-//   resolve();
-// })();
-
-// console.log("3");
-// (async () => {
-//   return await new Promise((resolve) => {
-//     crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
-//       console.log("P3");
-//       (async () => {
-//         return await new Promise((resolve) => {
-//           crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
-//             console.log("P3");
-//             (async () => {
-//               return await new Promise((resolve) => {
-//                 crypto.pbkdf2(
-//                   "12345678",
-//                   "12345678",
-//                   2000000,
-//                   64,
-//                   "sha512",
-//                   () => {
-//                     console.log("P3");
-//                     resolve();
-//                   }
-//                 );
-//               });
-//             })();
-//             resolve();
-//           });
-//         });
-//       })();
-//       resolve();
-//     });
-//   });
-// })();
-
-// console.log("4");
-// (async () => {
-//   return await new Promise((resolve) => {
-//     // crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
-//     //   console.log("P4");
-//     //   resolve();
-//     // });
-//     console.log("P1");
-//     resolve();
-//   });
-// })();
-
-(() => {
-  const count = 100;
-  let sum = 0;
-  for (let i = 0; i < count; ++i) {
-    sum += i + 1;
-  }
-  console.log(sum);
+const incrementHeight = (person) => {
+  const _person = person;
+  _person.height++;
+  return _person;
+};
+console.log("1");
+(async () => {
+  return await new Promise((resolve) => {
+    // crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
+    //   console.log("P1");
+    //   resolve();
+    // });
+    console.log("P1");
+    resolve();
+  });
 })();
+
+() => {
+  crypto.pbkdf2Sync();
+};
+
+console.log("2");
+(async () => {
+  return await new Promise((resolve) => {
+    // crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
+    //   console.log("P2");
+    //   resolve();
+    // });
+    console.log("P1");
+  });
+  resolve();
+})();
+
+console.log("3");
+(async () => {
+  return await new Promise((resolve) => {
+    crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
+      console.log("P3");
+      (async () => {
+        return await new Promise((resolve) => {
+          crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
+            console.log("P3");
+            (async () => {
+              return await new Promise((resolve) => {
+                crypto.pbkdf2(
+                  "12345678",
+                  "12345678",
+                  2000000,
+                  64,
+                  "sha512",
+                  () => {
+                    console.log("P3");
+                    resolve();
+                  }
+                );
+              });
+            })();
+            resolve();
+          });
+        });
+      })();
+      resolve();
+    });
+  });
+})();
+
+console.log("4");
+(async () => {
+  return await new Promise((resolve) => {
+    // crypto.pbkdf2("12345678", "12345678", 2000000, 64, "sha512", () => {
+    //   console.log("P4");
+    //   resolve();
+    // });
+    console.log("P1");
+    resolve();
+  });
+})();
+
+() => {
+  console.log("adsdasd");
+  console.log("adsdasd");
+  console.log("adsdasd");
+  console.log("adsdasd");
+  console.log("adsdasd");
+  console.log("adsdasd");
+};
